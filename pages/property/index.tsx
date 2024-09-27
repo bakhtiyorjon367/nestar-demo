16d3cd6ee84e3de360249288e72ed8878cba275f
bakhtiyorjon367 <bakhtee.0302@gmail.com>
2024-09-27 22:23:16 +0900

    feat: property list publishing

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,14 +1,55 @@
 import withLayoutBasic from "@/libs/components/layout/layoutBasic";
-import { Stack } from "@mui/material";
+import Filter from "@/libs/components/property/Filter";
+import PropertyCard from "@/libs/components/property/PropertyCard";
+import  KeyboardArrowDownRoundedIcon  from "@mui/icons-material/KeyboardArrowDownRounded";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import { NextPage } from "next";
+import React, { useState } from "react";
 
-const PropertyList = () =>{
+const PropertyList:NextPage = ({initialInput, ...props}: any) => {
+    const [properties, setProperties] = useState<number[]>(initialInput);
+
     return (
-            <div>
+            <div id="property-list-page" style={{position:'relative'}}>
                 <Stack className={"container"}>
-                    PROPERTY LIST
+                    <Box className={"right"}>
+                        <span>Sort by</span>
+                        <div>
+                            <Button endIcon={<KeyboardArrowDownRoundedIcon/>}>New</Button>
+                        </div>
+                    </Box>
+                    <Stack className={"property-page"}>
+                        <Stack className={"filter-config"}>
+                            <Filter/>
+                        </Stack>
+                        <Stack className={"main-config"} mb={"76px"}>
+                            <Stack className="list-config">
+                                {properties.map((property, index) => {
+                                    return <PropertyCard key={index}/>;
+                                })}
+                            </Stack>
+                            <Stack className={"pagination-config"}>
+                                <Stack className="pagination-box">
+                                    <Pagination 
+                                        page={1}
+                                        count={5}
+                                        shape="circular"
+                                        color="primary"
+                                    />
+                                </Stack>
+                                <Stack className="total-result">
+                                    <Typography> Total 5 properties available</Typography>
+                                </Stack>
+                            </Stack>
+                        </Stack>
+                    </Stack>
                 </Stack>
             </div>
         );
-}
+};
+
+PropertyList.defaultProps = {
+    initialInput:[1, 2, 3, 4, 5, 6],
+};
 
 export default withLayoutBasic(PropertyList);  // export default PropertyList; 
